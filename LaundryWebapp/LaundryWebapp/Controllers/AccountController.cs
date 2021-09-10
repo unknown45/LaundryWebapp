7f89e5e58f4023511e8586c11481b8cba94b28f9
Muhammad Husni <muhammad.husni105@gmail.com>
2021-09-10 14:46:54 +0700

    Nira Laundry UI V.16

--- LaundryWebapp/LaundryWebapp/Controllers/AccountController.cs
+++ LaundryWebapp/LaundryWebapp/Controllers/AccountController.cs
@@ -87,7 +87,7 @@ namespace LaundryWebapp.Controllers
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
                 default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
+                    ModelState.AddModelError("", "Email atau Password Salah.");
                     return View(model);
             }
         }
